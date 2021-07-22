6b4628e026b5219786c16d790bfaac1c697aa0f4
DaniGoodwin <danmarkhunter13072006@gmail.com>
2021-07-22 21:39:57 +0300

    some changes

--- src/native/appleskin/options.cpp
+++ src/native/appleskin/options.cpp
@@ -6,3 +6,6 @@ JS_MODULE_VERSION(ASOptionsJSAdapter, 1);
 JS_EXPORT(ASOptionsJSAdapter, getProfile, "I()", (JNIEnv* env){
     return GlobalContext::getMinecraftClient()->getOptions()->getUIProfile();
 });
+JS_EXPORT(ASOptionsJSAdapter, getGuiScale, "I()", (JNIEnv* env){
+    return GlobalContext::getMinecraftClient()->getGuiScaleIption()->getValue();
+});
