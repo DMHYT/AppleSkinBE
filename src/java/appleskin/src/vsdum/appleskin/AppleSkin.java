20336ff7da8c470a7695f500cff9c80903c7bc7d
vsdum <danmarkhunter13072006@gmail.com>
2021-10-11 20:50:21 +0300

    Finally managed with Java

--- src/java/appleskin/src/vsdum/appleskin/AppleSkin.java
+++ src/java/appleskin/src/vsdum/appleskin/AppleSkin.java
@@ -2,11 +2,13 @@ package vsdum.appleskin;
 
 import java.util.HashMap;
 
+import com.zhekasmirnov.innercore.api.mod.adaptedscript.AdaptedScriptAPI.Logger;
+
 public class AppleSkin {
     
     public static void boot(HashMap<?, ?> someData)
     {
-        
+        Logger.Log("AppleSkin", "Loading java...");
     }
 
 }
